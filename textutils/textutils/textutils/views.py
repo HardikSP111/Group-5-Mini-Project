71c03e9d431189003cf872239ac07c819547a567
Om <ombhoge1234@gmail.com>
2023-02-06 23:56:52 +0530

    Initial Commit

--- textutils/textutils/textutils/views.py
+++ textutils/textutils/textutils/views.py
@@ -1,1 +1,8 @@
 #I have created this file - Om bhoge
+from django.http import HttpResponse
+
+def index(request) : 
+    return HttpResponse("<h1>Om</h1>")
+
+def about(request) : 
+    return HttpResponse("About hello om")
